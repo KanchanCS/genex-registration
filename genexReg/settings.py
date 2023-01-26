88a21962ad0530d7d9786519f35cd3bbc8371b2c
Kanchan Yadav <61403194+KanchanCS@users.noreply.github.com>
2023-01-26 12:50:51 +0530

    Update settings.py

--- genexReg/settings.py
+++ genexReg/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'genex_db',
         'USER': 'root',
-        'PASSWORD': 'Kanchan!@1',
+        'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
